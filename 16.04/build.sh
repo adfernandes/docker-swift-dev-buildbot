d0b42c270a230d51df744b4c7d4f35e3c75aecb7
Shannon Noe <snoe925@gmail.com>
2017-01-21 08:58:45 -0600

    Add libdispatch build to build.sh

--- 16.04/build.sh
+++ 16.04/build.sh
@@ -6,4 +6,5 @@ exec /src/swift/utils/build-script \
   --preset=buildbot_linux \
   install_destdir=/install \
   installable_package=/output/swift-${TIMESTAMP}-ubuntu16.04.tar.gz \
+  --libdispatch -- --install-libdispatch
   2>&1 | tee /output/build-${TIMESTAMP}.log
