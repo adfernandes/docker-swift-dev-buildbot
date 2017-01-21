931db64e3724682997e550c0257e261d8efb901b
snoe925 <shannon_noe@yahoo.com>
2017-01-21 09:45:13 -0600

    Add support for using build.sh from /src/build.sh

--- 16.04/build.sh
+++ 16.04/build.sh
@@ -3,14 +3,13 @@
 TIMESTAMP=`date +"%Y%m%d-%H%M%S"`
 
 # Support using build.sh from external /src volume
-if [ -x /src/build.sh ]; then
-    CMD="/src/build.sh"
-else
-    # original behavior
-    CMD="/src/swift/utils/build-script"
+if [ "$0" == "/root/build.sh" -a -x /src/build.sh ]; then                
+   # If a build.sh script is present in the /src area, use that instead of the script in the
+   # container         
+   exec /src/build.sh  
 fi
 
-exec "$CMD" \
+exec /src/swift/utils/build-script \
   --preset=buildbot_linux \
   install_destdir=/install \
   installable_package=/output/swift-${TIMESTAMP}-ubuntu16.04.tar.gz \
