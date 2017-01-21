4fe1936be3c1dbb97ce31ddec3d3eb3936796323
Shannon Noe <snoe925@gmail.com>
2017-01-21 09:10:06 -0600

    Add ability to load build.sh from /src volume

--- 16.04/build.sh
+++ 16.04/build.sh
@@ -2,7 +2,15 @@
 
 TIMESTAMP=`date +"%Y%m%d-%H%M%S"`
 
-exec /src/swift/utils/build-script \
+# Support using build.sh from external /src volume
+if [ -x /src/build.sh ]; then
+    CMD="/src/build.sh"
+else
+    # original behavior
+    CMD="/src/swift/utils/build-script"
+fi
+
+exec "$CMD" \
   --preset=buildbot_linux \
   install_destdir=/install \
   installable_package=/output/swift-${TIMESTAMP}-ubuntu16.04.tar.gz \
