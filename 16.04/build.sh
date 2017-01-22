20b0b6877fe486f4ac97252d7a7252de33cdce75
Shannon Noe <snoe925@gmail.com>
2017-01-22 15:30:09 -0600

    Update build settings

--- 16.04/build.sh
+++ 16.04/build.sh
@@ -9,8 +9,14 @@ if [ "$0" == "/root/build.sh" -a -x /src/build.sh ]; then
    exec /src/build.sh  
 fi
 
+if [ ! -r /src/swift/utils/update-checkout ]; then
+    cd /src
+    git clone https://github.com/apple/swift.git
+    ./swift/utils/update-checkout --clone
+fi
+
 exec /src/swift/utils/build-script \
-  --preset=buildbot_linux \
+  --preset=buildbot_linux_1604 \
   install_destdir=/install \
   installable_package=/output/swift-${TIMESTAMP}-ubuntu16.04.tar.gz \
   2>&1 | tee /output/build-${TIMESTAMP}.log
